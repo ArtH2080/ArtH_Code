0fadd25e41a14f8ea77bf3807c28c8fa3b40240c
ArtH2080 <157556197+ArtH2080@users.noreply.github.com>
2024-09-07 22:03:31 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal é uma skatista brasileira que conquistou o mundo com seu talento precoce. Aos poucos anos, ela se tornou um ícone do esporte, colecionando títulos e quebrando recordes.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que vem se destacando no cenário internacional. Com sua técnica apurada e força, ela é uma das grandes promessas do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judoca Brasil esporte"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros e se tornou um dos maiores nomes do esporte nacional. Com sua força, determinação e talento, ela coleciona medalhas em competições internacionais, incluindo os Jogos Olímpicos.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica artística Olimpíadas Brasil medalhas"
+    }
+];
